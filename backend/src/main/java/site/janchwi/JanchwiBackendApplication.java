05a156ab6853700d362bc9577dea003c70d6ab6d
Spinichi <spinichi@naver.com>
2025-12-17 02:02:12 +0900

    chore(backend): Spring Boot 백엔드 초기 프로젝트 구성

---
+++ backend/src/main/java/site/janchwi/JanchwiBackendApplication.java
@@ -0,0 +1,13 @@
+package site.janchwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanchwiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanchwiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/site/janchwi/JanchwiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.janchwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanchwiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
